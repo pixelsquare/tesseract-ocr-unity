33f5cdaa550a0c3231863e42c802625336d2cc96
pixelsquare <anthony.ganzon@ymail.com>
2021-10-05 18:52:00 +0800

    Added new scenes for mobile camera demonstration (For android only) and image rotation test scene

---
+++ unity-project/Assets/Scripts/TesseractOCRCameraDemo.cs
@@ -0,0 +1,119 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="TesseractOCRCameraDemo.cs" company="PixelSquare">
+//    Copyright © 2021 PixelSquare
+// </copyright>
+// <author>Anthony G.</author>
+// <date>2021/10/04</date>
+// <summary>Tesseract OCR Camera Demonstration</summary>
+//------------------------------------------------------------------------------
+
+using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+using System.Collections.Generic;
+
+using TMPro;
+
+namespace PixelSquare
+{
+    using TesseractOCR;
+    using TesseractOCR.Utility;
+
+    /// <summary>
+    /// Tesseract OCR Camera Demonstration
+    /// </summary>
+    /// <remarks></remarks>
+    public class TesseractOCRCameraDemo : MonoBehaviour
+    {
+        /// <summary>
+        /// Language id
+        /// </summary>
+        [SerializeField] private string m_LanguageId = "eng+jpn";
+
+        /// <summary>
+        /// Sample image to extract text from
+        /// </summary>
+        [SerializeField] private RawImage m_SampleRawImage = null;
+
+        /// <summary>
+        /// Input field where we can edit the extracted text
+        /// </summary>
+        [SerializeField] private InputField m_InputField = null;
+
+        /// <summary>
+        /// Version Text
+        /// </summary>
+        [SerializeField] private TextMeshProUGUI m_VersionText = null;
+
+        private IOpticalCharacterReader m_Ocr = null;
+        private WebCamTexture m_WebCamTexture = null;
+        private Texture2D m_SnapshotTexture = null;
+
+        /// <summary>
+        /// Use this for initialization
+        /// </summary>
+        public IEnumerator Start()
+        {
+            WebCamDevice[] devices = WebCamTexture.devices;
+
+            for(int i = 0; i < devices.Length; i++)
+            {
+                Debug.Log(string.Format("Webcam Name: {0}", devices[i].name));
+
+                yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+
+                if(Application.HasUserAuthorization(UserAuthorization.WebCam))
+                {
+                    Debug.Log(string.Format("User granted webcam access. [{0}]", devices[i].name));
+                }
+            }
+
+            m_WebCamTexture = new WebCamTexture();
+            m_SampleRawImage.texture = m_WebCamTexture;
+            m_WebCamTexture.Play();
+
+            m_SampleRawImage.transform.rotation *= Quaternion.AngleAxis(m_WebCamTexture.videoRotationAngle, -Vector3.forward);
+
+            InitializeTesseract();
+        }
+
+        /// <summary>
+        /// Unity Event on game object destroy
+        /// </summary>
+        public void OnDestroy()
+        {
+            if(m_Ocr != null)
+            {
+                m_Ocr.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Initializes tesseract ocr library
+        /// </summary>
+        public void InitializeTesseract()
+        {
+            m_Ocr = new TesseractOCRImpl();
+            m_Ocr.Initialize(m_LanguageId);
+            m_VersionText.text = string.Format("Tesseract Version: {0}", m_Ocr.GetVersion());
+        }
+
+        /// <summary>
+        /// Converts image to text
+        /// </summary>
+        public void ConvertImageToText()
+        {
+            m_SnapshotTexture = new Texture2D(m_WebCamTexture.width, m_WebCamTexture.height, TextureFormat.RGB24, false);
+            Color32[] colorBuffer = TesseractOCRUtility.ImageRotateClockwise(m_WebCamTexture.GetPixels32(), m_WebCamTexture.width, m_WebCamTexture.height);
+
+            m_SnapshotTexture.Resize(m_WebCamTexture.height, m_WebCamTexture.width);
+            m_SnapshotTexture.SetPixels32(colorBuffer);
+            m_SnapshotTexture.Apply();
+
+            m_Ocr.SetImage(m_SnapshotTexture);
+            m_InputField.text = m_Ocr.GetText();
+        }
+    }
+
+} // namespace PixelSquare
---
+++ unity-project/Assets/Scripts/TesseractOCRImageRotation.cs
@@ -0,0 +1,103 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="TesseractOCRImageRotation.cs" company="PixelSquare">
+//    Copyright © 2021 PixelSquare
+// </copyright>
+// <author>Anthony G.</author>
+// <date>2021/10/05</date>
+// <summary>Tesseract OCR Image Rotation Demonstration</summary>
+//------------------------------------------------------------------------------
+
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Collections;
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PixelSquare
+{
+    /// <summary>
+    /// Tesseract OCR Image Rotation Demonstration
+    /// Used to check the algorithm for rotating and flipping image buffer
+    /// </summary>
+    /// <remarks></remarks>
+    public class TesseractOCRImageRotation : MonoBehaviour
+    {
+        /// <summary>
+        /// Sample texture image
+        /// </summary>
+        [SerializeField] private RawImage m_SampleImage = null;
+
+        /// <summary>
+        /// Rotates the image in clockwise direction
+        /// </summary>
+        public void RotateClockwise()
+        {
+            Texture2D sampleTex = (Texture2D)m_SampleImage.texture;
+            Color32[] colors = TesseractOCR.Utility.TesseractOCRUtility.ImageRotateClockwise(sampleTex.GetRawTextureData(), sampleTex.width, sampleTex.height);
+
+            Texture2D resultTex = new Texture2D(sampleTex.width, sampleTex.height, TextureFormat.RGB24, false);
+            resultTex.filterMode = FilterMode.Point;
+            resultTex.Resize(sampleTex.height, sampleTex.width);
+            resultTex.SetPixels32(colors);
+            resultTex.Apply();
+
+            m_SampleImage.texture = resultTex;
+            m_SampleImage.SetNativeSize();
+        }
+
+        /// <summary>
+        /// Rotates the image in counter clockwise direction
+        /// </summary>
+        public void RotateCounterClockwise()
+        {
+            Texture2D sampleTex = (Texture2D)m_SampleImage.texture;
+            Color32[] colors = TesseractOCR.Utility.TesseractOCRUtility.ImageRotateCounterClockwise(sampleTex.GetRawTextureData(), sampleTex.width, sampleTex.height);
+
+            Texture2D resultTex = new Texture2D(sampleTex.width, sampleTex.height, TextureFormat.RGB24, false);
+            resultTex.filterMode = FilterMode.Point;
+            resultTex.Resize(sampleTex.height, sampleTex.width);
+            resultTex.SetPixels32(colors);
+            resultTex.Apply();
+
+            m_SampleImage.texture = resultTex;
+            m_SampleImage.SetNativeSize();
+        }
+
+        /// <summary>
+        /// Flips the image vertically
+        /// </summary>
+        public void FlipVertical()
+        {
+            Texture2D sampleTex = (Texture2D)m_SampleImage.texture;
+            Color32[] colors = TesseractOCR.Utility.TesseractOCRUtility.ImageFlipVertical(sampleTex.GetRawTextureData(), sampleTex.width, sampleTex.height);
+
+            Texture2D resultTex = new Texture2D(sampleTex.width, sampleTex.height, TextureFormat.RGB24, false);
+            resultTex.filterMode = FilterMode.Point;
+            resultTex.SetPixels32(colors);
+            resultTex.Apply();
+
+            m_SampleImage.texture = resultTex;
+            m_SampleImage.SetNativeSize();
+        }
+
+        /// <summary>
+        /// Flips the image horizontally
+        /// </summary>
+        public void FlipHorizontal()
+        {
+            Texture2D sampleTex = (Texture2D)m_SampleImage.texture;
+            Color32[] colors = TesseractOCR.Utility.TesseractOCRUtility.ImageFlipHorizontal(sampleTex.GetRawTextureData(), sampleTex.width, sampleTex.height);
+
+            Texture2D resultTex = new Texture2D(sampleTex.width, sampleTex.height, TextureFormat.RGB24, false);
+            resultTex.filterMode = FilterMode.Point;
+            resultTex.SetPixels32(colors);
+            resultTex.Apply();
+
+            m_SampleImage.texture = resultTex;
+            m_SampleImage.SetNativeSize();
+        }
+
+    }
+
+} // namespace PixelSquare
